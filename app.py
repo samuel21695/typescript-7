6610f1dd87d5d977f73dfe861d011842d3eaf939
samuel21695 <samuel21695@gmail.com>
2023-12-18 19:38:01 +0900

    📝 python error log

---
+++ app.py
@@ -0,0 +1,8 @@
+def addPy(a, b):
+  if(type(a) != int or type(b) != int):
+    raise TypeError('a and b must be integers')
+  return a + b
+
+print(addPy(1, 2)) #3
+print(addPy('1', '2'))
+# TypeError: a and b must be intergers
